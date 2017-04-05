1558d350bef2334091e779673959a4f7284961e9
root <root@kickstart.olivermel.com>
2017-04-05 06:12:34 -0700

    more tutorials

---
+++ ex11-input.py
@@ -0,0 +1,21 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." %(age, height, weight)
+
+# the comma at the end of the print lines tell python to continue on to next line
+
+print "When last did you have your first kiss?",
+kiss = raw_input()
+
+print "How good was the kiss?",
+good = raw_input()
+
+print "What was the name of the person you kissed?",
+person = raw_input()
+
+print "So because the kiss %r was %r that led you to want to smash this %r?." %(kiss, good, person)
